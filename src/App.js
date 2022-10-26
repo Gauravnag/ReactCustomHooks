eadbda8d1f9ac000e2b2066811cd53991b390d68
Gaurav <gauravnag28@gmail.com>
2022-10-26 11:41:45 +0530

    Text appear on click of submit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [isError, setIsError] = useState("");
-
-  // Use Fetch Await Method
-  const getApiData = async () => {
-    try {
-      const apiData = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const apiResp = await apiData.json();
-      setData(apiResp);
-    } catch(error) {
-      console.log(error);
-    } 
-  }
-
-  useEffect(() => {
-    getApiData();
-  }, [])
-
+    const [name, setName] = useState("Gnag");
+    const [txtSub, setTxtSub] = useState("");
+    // On input field change
+    const InputEvent = (event) => {
+      setName(event.target.value);
+    }
+    // On click of btn
+    const onSubmit = () => {
+      setTxtSub(name);
+    }
+    // Txt color
+    const txtColor = {
+      color: "blue"
+    }
     return(
         <>
-          <h2>Axios Data Fetch</h2>
-          {
-            data.slice(0,15).map((curr) => {
-              const { id, title, body } = curr;
-              return(
-                <div key={id} style={{width: "400px", border: "1px solid #000" }}>
-                  <h4>Title: {title.slice(0,15).toUpperCase()}</h4>
-                  <p> {body.slice(0,100)} </p>
-                </div>
-              )
-            })
-          }
-
-          {/* {isError !== "" && <h2 style={{color: "red"}}> {isError} </h2> } */}
+          <h3>Form Submit onClick</h3>
+          <input type="text" value={name} onChange={InputEvent} /> 
+          <br /><br />
+          <button onClick={onSubmit}>Submit</button>
+          <hr />
+          <p> Before Submit: <span style={txtColor}>{name}</span>  & 
+          Text After submit btn clicked: <span style={txtColor}>{txtSub}</span>  </p>
         </>
     )
 }
