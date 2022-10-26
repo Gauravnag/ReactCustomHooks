1748ce5351c720b51b8d59c7e9790c23003096e9
Gaurav <gauravnag28@gmail.com>
2022-10-26 13:12:40 +0530

    Form Data Using SPread Operator

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
 import { useState } from "react";
 
 const App = () => {
-    const [name, setName] = useState("Gnag");
-    const [txtSub, setTxtSub] = useState("");
-    // On input field change
-    const InputEvent = (event) => {
-      setName(event.target.value);
-    }
-    // On click of btn
-    const onSubmit = () => {
-      setTxtSub(name);
-    }
-    // Txt color
-    const txtColor = {
-      color: "blue"
-    }
+  // Store variable name
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    phone: "",
+  });
+  // Input field Text change
+  const inputEvent = (event) => {
+    setFullName((preVal) => {
+      const {name, value} = event.target;
+      return {
+        ...preVal,
+        [name]: value,
+      }
+    });
+  }
+  // Text color
+  const txtColor = {
+    color: "blue",
+  }
+    
     return(
         <>
-          <h3>Form Submit onClick</h3>
-          <input type="text" value={name} onChange={InputEvent} /> 
-          <br /><br />
-          <button onClick={onSubmit}>Submit</button>
-          <hr />
-          <p> Before Submit: <span style={txtColor}>{name}</span>  & 
-          Text After submit btn clicked: <span style={txtColor}>{txtSub}</span>  </p>
+          <h3>Form based on spread operator</h3>
+          <input 
+            type="text" 
+            name="fname" 
+            value={fullName.fname} 
+            onChange={inputEvent} 
+            placeholder="First Name"
+            />
+
+          <input 
+            type="text"
+            name="lname"
+            value={fullName.lname}
+            onChange={inputEvent}
+            placeholder="Last Name"
+          />
+
+          <input 
+            type="email" 
+            name="email"
+            value={fullName.email}
+            onChange={inputEvent}
+            placeholder="Email"
+          />
+
+          <input 
+            type="phone"
+            name="phone"
+            value={fullName.phone}
+            onChange={inputEvent}
+            placeholder="Phone no."
+          />
+
+           <hr />
+           <p> Full name: <span style={txtColor}>{fullName.fname}</span> & Last name: <span style={txtColor}>{fullName.lname}</span> </p>
+           <p> Email ID: <span style={txtColor}> {fullName.email} </span> </p>
+           <p> Phone no.: <span style={txtColor}> {fullName.phone} </span> </p>
         </>
     )
 }
