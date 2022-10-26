4e0dc06bf9d33d8b44b9295ce3cf5ee12208c1ab
Gaurav <gauravnag28@gmail.com>
2022-10-26 14:19:50 +0530

    Form Data submit on btn click

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
     email: "",
     phone: "",
   });
+  const [records, setRecords] = useState([]);
   // Input field Text change
   const inputEvent = (event) => {
     setFullName((preVal) => {
@@ -22,46 +23,65 @@ const App = () => {
   const txtColor = {
     color: "blue",
   }
+  // Submit Button
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newRecord = {...fullName, id: new Date().getTime().toString() }
+    setRecords([...records, newRecord]);
+  }
     
     return(
         <>
           <h3>Form based on spread operator</h3>
-          <input 
-            type="text" 
-            name="fname" 
-            value={fullName.fname} 
-            onChange={inputEvent} 
-            placeholder="First Name"
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              name="fname" 
+              value={fullName.fname} 
+              onChange={inputEvent} 
+              placeholder="First Name"
+              />
+
+            <input 
+              type="text"
+              name="lname"
+              value={fullName.lname}
+              onChange={inputEvent}
+              placeholder="Last Name"
             />
 
-          <input 
-            type="text"
-            name="lname"
-            value={fullName.lname}
-            onChange={inputEvent}
-            placeholder="Last Name"
-          />
+            <input 
+              type="email" 
+              name="email"
+              value={fullName.email}
+              onChange={inputEvent}
+              placeholder="Email"
+            />
 
-          <input 
-            type="email" 
-            name="email"
-            value={fullName.email}
-            onChange={inputEvent}
-            placeholder="Email"
-          />
-
-          <input 
-            type="phone"
-            name="phone"
-            value={fullName.phone}
-            onChange={inputEvent}
-            placeholder="Phone no."
-          />
+            <input 
+              type="phone"
+              name="phone"
+              value={fullName.phone}
+              onChange={inputEvent}
+              placeholder="Phone no."
+            />
+            <button type="submit">Submit</button>
+          </form>
 
            <hr />
-           <p> Full name: <span style={txtColor}>{fullName.fname}</span> & Last name: <span style={txtColor}>{fullName.lname}</span> </p>
-           <p> Email ID: <span style={txtColor}> {fullName.email} </span> </p>
-           <p> Phone no.: <span style={txtColor}> {fullName.phone} </span> </p>
+           {
+            records.map((curr) => {
+              const { fname, lname, email, phone } = curr;
+              return(
+                <div key={curr.id}>
+                  <p> First name: <span style={txtColor}>{fname}</span> & 
+                      Last name: <span style={txtColor}> {lname} </span></p>
+                  <p> Email: <span style={txtColor}> {email} </span> </p>
+                  <p> Phone: <span style={txtColor}> {phone} </span> </p>
+                </div>
+              )
+            })
+           }
         </>
     )
 }
