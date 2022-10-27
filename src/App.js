f4e63e1614401f8d4937378bc92ca78e72f891f3
Gaurav <gauravnag28@gmail.com>
2022-10-27 16:09:27 +0530

    Clean up memory on resize

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { useState } from "react";
-import Api from "./Api";
-import Accordion from "./Accordion";
+import { useEffect, useState } from "react";
 
 const App = () => {
-    const [data, setData] = useState(Api);
+    // Store The main width of the window
+    const [widthCount, setWidthCount] = useState(window.screen.width);
+    // Width change based on window resize
+    const actualWidth = () => {
+      // Store the change width of width after resize
+      setWidthCount(window.innerWidth);
+      console.log(window.innerWidth);
+    }
+   
+    useEffect(() => {
+      // Inspect Element Tab -> Event Listeners
+        // Addng to memory 
+        window.addEventListener("resize", actualWidth);
+        
+        return () => {
+          // Removing from memory 
+          window.removeEventListener("resize", actualWidth);
+        }
+    })
     return(
         <>
-          <h3>Accordion</h3>
-          {
-            data.map((curr) => {
-              return <Accordion key={curr.id} {...curr} />
-            })
-          }
+         <h3>Cleanup Function</h3>
+         <p> {widthCount} </p>
         </>
     )
 }
